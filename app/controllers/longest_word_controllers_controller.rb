a4c665f36d618637ac8d3ec8ef1ab9b2c4cef821
Anthony Zamarro <anthony.zamarro@gmail.com>
2017-11-27 21:30:01 +0100

    longest word game

---
+++ app/controllers/longest_word_controllers_controller.rb
@@ -0,0 +1,62 @@
+require 'open-uri'
+require 'json'
+
+class LongestWordControllersController < ApplicationController
+  def game
+    @grid = generate_grid(9)
+    @start = Time.now
+  end
+
+  def score
+    @guess = params[:guess]
+    @start_time = params[:start].to_i
+    @end_time = Time.now.to_i
+    @grid = params[:grid]
+
+    # parse time from string to integer
+    @results = run_game(@guess, @grid, @start_time, @end_time)
+  end
+
+  private
+
+  def generate_grid(grid_score)
+    Array.new(grid_score) { ('A'..'Z').to_a.sample }.join(' ')
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { end_time: end_time, start_time: start_time }
+
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+    result
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    attempt
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
